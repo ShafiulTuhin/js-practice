c70ea5ea3a7c8a95172a835b0c8e2407f9e1117d
ShafiulTuhin <tuhin6488@gmail.com>
2026-01-03 01:01:38 +0600

    Reverse name

--- script.js
+++ script.js
@@ -6,3 +6,9 @@ for (i = 1; i <= 3; i++) {
     console.log(`Do exercise number - ${j} in the morning`);
   }
 }
+
+//--> Reverse name
+let name = "Tuhin";
+let reversedName = name.split("").reverse().join("");
+
+console.log(reversedName);
